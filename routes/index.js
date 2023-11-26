900a5166489b374b2b43bc375f8dceeb6409ccfd
fazriegi <fazriegi80@gmail.com>
2023-11-26 12:45:26 +0700

    add list doctor

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const route = express.Router();
 
 const authRoute = require("./auth-route");
 const discussionRoute = require("./discussion-route");
+const doctorRoute = require("./doctor-route");
 
 route.use("/auth", authRoute);
 route.use("/discussions", discussionRoute);
+route.use("/doctors", doctorRoute);
 
 module.exports = route;
